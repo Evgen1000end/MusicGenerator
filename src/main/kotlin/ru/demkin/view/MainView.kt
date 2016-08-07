7d15f7c28843e82c58846bbb3e13ef91b3e44964
evgen1000end <evgen1000end@gmail.com>
2016-08-07 10:21:31 +0300

    Added simple TornadoFX UI

---
+++ src/main/kotlin/ru/demkin/view/MainView.kt
@@ -0,0 +1,21 @@
+package ru.demkin.view
+
+import ru.demkin.app.Styles.Companion.heading
+import javafx.scene.layout.HBox
+import tornadofx.View
+import tornadofx.addClass
+import tornadofx.label
+
+class MainView : View() {
+    override val root = HBox()
+
+    init {
+        title = "Hello TornadoFX Application"
+
+        with (root) {
+            label(title) {
+                addClass(heading)
+            }
+        }
+    }
+}
